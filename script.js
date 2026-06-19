13fd46c230abd51476660abd51491769a07ed676
harshitchauhan13-crypto <harshitchauan13@gmail.com>
2026-06-19 10:30:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,107 @@
+
+const createBtn = document.querySelector("#create");
+const formDiv = document.querySelector(".form");
+const closeBtn = document.querySelector("#close");
+const form = document.querySelector("form");
+const productDiv = document.querySelector(".product");
+const productArr = [];
+
+let updateIndex = null;
+
+let ui = () => {
+    productDiv.innerHTML = "";
+    productArr.forEach((elem,index) => {
+        productDiv.innerHTML += ` <div class="product-card">
+            <div class="img">
+                <img src="${elem.image}" alt="">
+            </div>
+
+            <div class="text">
+                <h3>${elem.productName}</h3>
+                <p>${elem.description}</p>
+                <p>${elem.price}</p>
+            </div>
+
+            <div class="btns">
+                <button onclick="updateProduct('${elem.productName}')" id="update">Update</button>
+                <button onclick="deleteProduct(${index})" id="delete">Delete</button>
+            </div>
+        </div>
+        `;
+
+    });
+};
+
+
+
+createBtn.addEventListener("click", () => {
+    formDiv.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+    formDiv.style.display = "none";
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let productName = event.target[0].value;
+    let description = event.target[1].value;
+    let price = event.target[2].value;
+    let image = event.target[3].value;
+
+    if(productName.trim() === "" ||
+       description.trim() === "" ||
+       price.trim() === "" ||
+       image.trim() === "" ) {
+        alert("please fill all the fields");
+        return;
+    }
+
+    let obj ={
+        productName,
+        description,
+        price,
+        image,
+    };
+
+    if (updateIndex !== null) {
+        productArr[updateIndex] = obj;
+        updateIndex = null;
+    } else {
+         productArr.push(obj);
+        }
+
+    ui();
+
+    console.log(productArr)
+
+    form.reset();
+
+     formDiv.style.display = "none";
+});
+
+
+
+
+function updateProduct(name) {
+    console.log(name);
+
+     formDiv.style.display = "flex";
+    let product = productArr.find((elem) => elem.productName === name);
+    updateIndex = productArr.findIndex((elem) => elem.productName === name);
+
+    form[0].value = product.productName;
+    form[1].value = product.description;
+    form[2].value = product.price;
+    form[3].value = product.image;
+    
+}
+
+function deleteProduct(index) {
+    productArr.splice(index, 1);
+    ui();
+}
+
+
+
